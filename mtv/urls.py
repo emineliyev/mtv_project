4bddfccab797a23cc1b566650d64b8c6aac5570e
emin <emineliyev87@gmail.com>
2022-08-17 00:05:08 +0400

    Commit for deploy heroku 5

--- mtv/urls.py
+++ mtv/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('', include('post.urls')),
     path('', include('account.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
