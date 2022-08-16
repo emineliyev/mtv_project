adfda90fc9d78935ad51a24727797b3125a46013
emin <emineliyev87@gmail.com>
2022-08-17 02:45:46 +0400

    Media static

--- mtv/urls.py
+++ mtv/urls.py
@@ -23,11 +23,8 @@ urlpatterns = [
     path('', include('post.urls')),
     path('', include('account.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
