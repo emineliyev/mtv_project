ca3ab39be3de87270b160e8dd528228dfe0a93a5
emin <emineliyev87@gmail.com>
2022-08-17 19:57:54 +0400

    settings.DEBUG reload

--- mtv/urls.py
+++ mtv/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
